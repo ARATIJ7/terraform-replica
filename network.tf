eae25705c534ce391adad1806a563e29123c006f
Arati Jagtap <154422939+ARATIJ7@users.noreply.github.com>
2024-06-12 13:16:33 +0530

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.region}a"
+}
+
+resource "aws_security_group" "mongodb_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
